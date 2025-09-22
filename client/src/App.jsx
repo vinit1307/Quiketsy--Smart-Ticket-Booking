64c00ce8792f52e105074755bc7e2306ac052f19
vivekkkpatel <vivek2530patel@gmail.com>
2025-09-22 21:36:29 +0530

    Added Carousel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,35 @@
 import React from "react";
-import { useState } from 'react'
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
+import Carousel from "../components/Carousel";
 
 function App() {
-
   return (
     <>
       {/* <p class="bg-amber-400">Hello</p> */}
       {/* <Navbar></Navbar> */}
       <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        {/* You can add more pages here */}
-      </Routes>
-    </Router>
+        <Navbar />
+        <div className="mt-6 w-full">
+          <Carousel
+            autoplay={true}
+            autoplayDelay={3000}
+            pauseOnHover={true}
+            loop={true}
+            round={false}
+          />
+        </div>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          {/* You can add more pages here */}
+        </Routes>
+      </Router>
     </>
   );
 }
 
-export default App
+export default App;
