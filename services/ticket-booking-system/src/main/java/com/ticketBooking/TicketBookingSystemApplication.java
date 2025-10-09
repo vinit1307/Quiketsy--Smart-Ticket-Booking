8217529b72176cefb6e7e30eab13164760e66701
vinit1307 <vinitmalwe48@gmail.com>
2025-10-09 09:51:33 +0530

    Secured the Database Password and the JWT Key and Deplyed the Database on Supabase

--- services/ticket-booking-system/src/main/java/com/ticketBooking/TicketBookingSystemApplication.java
+++ services/ticket-booking-system/src/main/java/com/ticketBooking/TicketBookingSystemApplication.java
@@ -3,11 +3,18 @@ package com.ticketBooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class TicketBookingSystemApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
 		SpringApplication.run(TicketBookingSystemApplication.class, args);
 	}
 }
--- services/ticket-booking-system/src/main/java/com/ticketBooking/security/JwtUtil.java
+++ services/ticket-booking-system/src/main/java/com/ticketBooking/security/JwtUtil.java
@@ -2,6 +2,11 @@ package com.ticketBooking.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -14,11 +19,23 @@ public class JwtUtil {
     // private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     // Or use a fixed long secret string (recommended for production)
-    private static final String SECRET = "mysuperlongsecuresecretmysuperlongsecuresecretmysuperlongsecuresecret123";
-    private static final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    //private static final String SECRET = "mysuperlongsecuresecretmysuperlongsecuresecretmysuperlongsecuresecret123";
+   // private static final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
 
-    private final long expirationMs = 86400000; // 1 day
+    //private final long expirationMs = 86400000; // 1 day
 
+    @Value("${jwt.secret}")
+    private String SECRET;
+
+    private Key key;
+
+    @Value("${jwt.expirationMs}")
+    private long expirationMs;
+
+    @PostConstruct
+    public void init() {
+        key  = Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
     // Generate JWT token
     public String generateToken(String email) {
         return Jwts.builder()
