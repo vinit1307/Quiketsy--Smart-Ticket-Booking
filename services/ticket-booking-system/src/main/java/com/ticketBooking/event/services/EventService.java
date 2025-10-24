2f36db0a64bc19c3d198542d482222bf8f350cf3
Vidhan-Tayade <vidhantayade@gmail.com>
2025-10-24 21:25:08 +0530

    all cities filtration

---
+++ services/ticket-booking-system/src/main/java/com/ticketBooking/event/services/EventService.java
@@ -0,0 +1,61 @@
+package com.ticketBooking.event.services;
+
+import com.ticketBooking.event.model.Event;
+import com.ticketBooking.event.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    // Get all events
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    // Get all unique cities
+   // public List<String> getAllCities() {
+    //    return eventRepository.findAllUniqueCities();
+    //}
+
+    // Get events by city
+    public List<Event> getEventsByCity(String city) {
+        if (city == null || city.trim().isEmpty()) {
+            return getAllEvents(); // Return all if city is empty
+        }
+        String s1=city.toLowerCase();
+        return eventRepository.findByCityIgnoreCase(s1);
+
+    }
+
+    // Get trending events
+    public List<Event> getTrendingEvents() {
+        return eventRepository.findByIsTrendingTrue();
+    }
+
+    // Get events by category
+    public List<Event> getEventsByCategory(String category) {
+        return eventRepository.findByCategory(category);
+    }
+
+    // // Search events
+    // public List<Event> searchEvents(String keyword) {
+    //     return eventRepository.searchEvents(keyword);
+    // }
+
+    // Get event by ID
+    public Event getEventById(UUID eventId) {
+        return eventRepository.findById(eventId).orElse(null);
+    }
+
+    // Get events by organizer
+    public List<Event> getEventsByOrganizer(Integer organizerId) {
+        return eventRepository.findByOrganizerId(organizerId);
+    }
+}
