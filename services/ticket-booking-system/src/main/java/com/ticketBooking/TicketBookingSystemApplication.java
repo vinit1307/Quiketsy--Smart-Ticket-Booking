f38570128ed2277127823ffcddf7af2dc32af0d7
vinit1307 <vinitmalwe48@gmail.com>
2025-12-07 19:06:43 +0530

    Changes in the main class

--- services/ticket-booking-system/src/main/java/com/ticketBooking/TicketBookingSystemApplication.java
+++ services/ticket-booking-system/src/main/java/com/ticketBooking/TicketBookingSystemApplication.java
@@ -11,10 +11,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class TicketBookingSystemApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASS	WORD", dotenv.get("DB_PASSWORD"));
+		// Dotenv dotenv = Dotenv.load();
+		// System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		// System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		// System.setProperty("DB_PASS	WORD", dotenv.get("DB_PASSWORD"));
 		// System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 		// System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
 		SpringApplication.run(TicketBookingSystemApplication.class, args);
